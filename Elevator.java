daedff80727da3812d62cd3a6d555ad76bac2491
clivejj <cjohnston1@stuy.edu>
2017-05-22 21:51:52 -0400

    Updated Elevator

--- Elevator.java
+++ Elevator.java
@@ -1,50 +1,86 @@
 import java.util.*;
-     public class Elevator{
-        private ArrayPriorityQueue<Integer> riders;
-        private int timeToEnd=0;
-        //private boolean available=true
-        //private String range;
-        private int currFloor=1;
-        private int maxFloor;
-        private boolean returning=false;
-        private ArrayList<Integer> floorsRemaining; //weird stuff here and below
-        private ArrayList<Integer> allFloors;
-         
-    public Elevator(ArrayList<Integer> passengers){
-        for (Integer i: passengers)
-            riders.add(i); //built by ontrol tower
-    }
-         
-    public void setRange(String newRange){
-        range = newRange; //to be done by control tower
-    }
-         
-    public ArrayList<Integer> findFloors
+public class Elevator{
     
-    public void calcTime(){
+    private ArrayPriorityQueue<Integer> riders;
+    private int timeToEnd;
+    private boolean available;
+    //ints for min and max floors Elevator will cover
+    private int minZone;
+    private int maxZone;
+    
+    private int currFloor;
+    
+    //max floor Elevator will need to go to (based on its Passengers)
+    private int maxFloor;
+    
+    //true if Elevator is going down
+    private boolean returning;
+    
+    //private ArrayList<Integer> floorsRemaining; //weird stuff here and below
+    //private ArrayList<Integer> allFloors;
+
+    //number of unique floors Elevator will need to go to (based on its Passengers)
+    private int numFloors;
+    //number of Passengers in riders
+    private int numPassengers;
+
+    public Elevator() {
+	riders = new ArrayPrioirtyQueue<Integer>();
+	timeToEnd = 0;
+	available = true;
+	currFloor = 0;
+	returning = false;
+	numFloors = 0;
+	numPassengers = 0;
+    }
+
+    //overloaded constructor for when passengers are already known
+    public Elevator(ArrayList<Integer> passengers){
+	this();
+        for (Integer i: passengers)
+            riders.add(i); //built by control tower
+    }
+         
+    public void setRange(int setMinZone, int setMaxZone) {
+	minZone = setMinZone;
+	maxZone = setMaxZone;
+        
+    }
+         
+    //public ArrayList<Integer> findFloors
+
+    //calulates how much time it will take Elevator to reach ground floor
+    public int calcTime(){
         if (returning)
-            timeToEnd = maxFloor * 4; //4 sec for every floor
+            timeToEnd =  currFloor * 4; //4 sec for every floor
         else
-            timeToEnd = floorsRemaining.getSize() * 4 + maxFloor * 4;
-        return timetoEnd;
+            timeToEnd = ((maxFloor - currFloor) * 4) + //4 sec for every floor until it gets to maxFloor
+		(maxFloor * 4) //4 sec for every floor it has to gone down from maxFloor to ground
+		+ numPassengers //1 sec for every Passenger getting off their floor	
+		return timetoEnd;
     }
             
     public String toString(){
         return "Time it will take: " + timeToEnd + "Floors that will be visited this trip: " + allFloors;
     }
-    public void add(Integer a){
-        riders.add(a)
+    
+    public Integer add(Integer a){
+        riders.add(a);
+	return a;
+	
     }
+    
     public void remove(){
         currFloor = riders.peekMin();
         while (riders.peekMin() == currFloor)
             riders.removeMin();
     }
+
+    public boolean isEmpty() {
+	return numPassengers == 0;
+    }
         
-         
-         
-         //move method??
-            
-        
-    
-}
+ 
+    //move method??
+           
+}//end class Elevator
