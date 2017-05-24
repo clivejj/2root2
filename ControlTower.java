a2a37bf57b8f8e25f8abfd4133f8d3ce99b79e5a
Caleb Smith-Salzberg <csmithsalzberg@stuy.edu>
2017-05-23 22:52:00 -0400

    Update ControlTower.java

--- ControlTower.java
+++ ControlTower.java
@@ -13,11 +13,46 @@ public class ControlTower {
 	    people.add((int)(Math.random() * 11));
 	}
 	ellies = new ArrayList<Elevator>();
+	for (int i = 0; i < 5; i++){
+            Elevator i = new Elevator();
+	    ellies.add(i);
+	}
     }
 
     public assignMinMax() {
-	min = people.min();
-    
+	min = Collections.min(people);
+	max = Collections.max(people);
+    }
+	
+    public void assignRanges() {
+    	double floorsPerEl = (max-min)/ellies.getSize(); 
+	if ((int)floorsPerEl == floorsPerEl){    //if floors are divisible by num of elevators
+	for (int i = 0; i < ellies.getSize(); i++){
+	    if (i==0)
+		ellies.get(i).setRange(min,min+floorsPerEl-1);
+	    else{
+		int prevEl = ellies.get(i-1).getMaxFloor();
+		ellies.get(i).setRange(prevEl+1, prevEl+floorsPerEl);
+	    }
+	}
+	}
+	else{
+	    int more = (int)(floorsPerEl+1);
+	    for (int i = 0; i < ellies.getSize(); i++){
+	    if (i==0)
+		ellies.get(i).setRange(min,min+more-1);
+	    else{
+		int prevEl = ellies.get(i-1).getMaxFloor();
+		ellies.get(i).setRange(prevEl+1, prevEl+more);
+		if ((int)(max-(prevEl+more)/(ellies.getSize()-i-1) == max-(prevEl+more)/(ellies.getSize()-i-1)){
+			more-=1;
+		}	
+	    }
+	}
+	}
+    }
+
+				       
 
     
 
